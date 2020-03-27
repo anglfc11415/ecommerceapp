bd023dee7239fda8e72d8ce6cc287593a20bce61
Vania Nettleford <vanianettleford@Vanias-MacBook-Pro-2.local>
2020-03-27 13:34:14 -0400

    update menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import HomePgae from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+<div>
+  <h1>HATS PAGE </h1>
+</div>
+)
 function App() {
   return (
-   <HomePgae/>
+    <div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop/:productCategory' component={HatsPage} />
+      <Route path='/products/:productId' component={HatsPage} />
+    </Switch>
+    </div>
   );
 }
 
